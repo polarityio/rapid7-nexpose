cb3bf1bae57770e9635fc507f78349a160c69e73
Jon Penwood <jonp@polarity.io>
2023-01-23 15:37:56 -0600

    chore: Added defaultColor to config files

--- config/config.js
+++ config/config.js
@@ -25,6 +25,7 @@ module.exports = {
   description:
     "Rapid7's on-premise option for vulnerability management software, monitors exposures in real-time",
   entityTypes: ['ipv4', 'cve', 'ipv6'],
+  defaultColor: 'light-pink',
   /**
    * An array of style files (css or less) that will be included for your integration. Any styles specified in
    * the below files can be used in your custom template.
